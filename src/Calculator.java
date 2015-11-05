03916fe7a8703c6104eea117626bb3d7d4575d6d
Robbie S <robbie@selwynsoftware.com>
2015-11-04 20:32:23 -0800

    created the frame and started working

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.math.*;
+public class Calculator {
+	public Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	}
+
+}
