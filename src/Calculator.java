fffbe6b98bb03a3e9e31e2c07f1ec73d80fb5b0d
Robbie S <robbie@selwynsoftware.com>
2015-11-04 20:33:18 -0800

    fixed a mixup

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 	public Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+		
 	}
 
 }
--- src/LinearFactor.java
+++ src/LinearFactor.java
@@ -1,4 +1,24 @@
 
 public class LinearFactor {
-
+	public float factor;
+	public char varname;
+	
+	public void setVar(char var){
+		varname = var;
+	}
+	public char getVar(){
+		return varname;
+	}
+	
+	public void setCoeff(float factor){
+		this.factor = factor;
+	}
+	
+	public float getFactor(){
+		return factor;
+	}
+	
+	public void ParseFactor(String expr, char var){
+		
+	}
 }
