1c9954e1218349a275f23e6ad32c68164c175a06
Robbie S <robbie@selwynsoftware.com>
2015-11-08 21:28:18 -0800

    entry works now for polynomials, still working on parseing

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -5,6 +5,15 @@ public class Polynomial {
 	public int degree = 0;
 	private ArrayList <Float> coefficients = new ArrayList<Float>();
 	private Scanner scan = new Scanner(System.in);
+	
+	public void setdegree(int deg){
+		this.degree = deg;
+	}
+	
+	public int getDegree(){
+		return this.degree;
+	}
+	
 	public void setPoly(String text){
 		this.polynomial = text;
 	}
@@ -21,14 +30,29 @@ public class Polynomial {
 	public void parseInput(){
 		
 		String newPoly = this.polynomial.replace("-", "+-");
-		String[] arr = polynomial.split("+");
+		String[] arr = polynomial.split("\\+",10);
+		System.out.println(arr);
+		setdegree(getDegree(arr[0]));
 		for (int i = 0; i < arr.length; i++){
-			
+			if(getDegree(arr[i]) == this.degree - i){
+				coefficients.add(getCoeffFromString(arr[i]));
+			}
+			else{
+				coefficients.add((float) 0);
+			}
 		}
-		
+		for (Float temp : coefficients) {
+			System.out.println(temp);
+		}
 	}
 	
+	public int getDegree(String input){
+		char asC = input.charAt(input.length()-1);
+		return (int) asC;
+	}
 	
-	
+	public float getCoeffFromString(String text){
+		return text.charAt(0);
+	}
 	
 }
