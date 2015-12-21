f1a1230969bd7f63f6665fcce11c360d355496b1
Robbie S <robbie@selwynsoftware.com>
2015-12-20 18:19:49 -0800

    started working on a GUI

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,16 @@
 import java.util.*;
+
+import ui.*;
+
 import java.math.*;
-public class Calculator {
+public class Calculator{
 	public static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		MainWin mainWindow = new MainWin();
+		
 		LinearFactor lf = new LinearFactor();
 		Polynomial poly = new Polynomial();
 		lf.getNewLinearFactor();
