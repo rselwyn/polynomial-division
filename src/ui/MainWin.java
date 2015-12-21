43e2677e3c3c649bde8a644a0185f982a7d4cce2
Robbie S <robbie@selwynsoftware.com>
2015-12-20 20:50:42 -0800

    more work on the ui.  The home page is almost done

--- src/ui/MainWin.java
+++ src/ui/MainWin.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -9,7 +11,8 @@ public class MainWin extends JFrame{
 	public MainWin() throws InterruptedException{
 		startWin();
 		mainScreenPanelMake();
-		
+		startScreenPanelMake();
+		addHomePanels();
 		window.show();
 		
 		
@@ -33,6 +36,8 @@ public class MainWin extends JFrame{
 	
 	private final int HEIGHT = 300;
 	private final int WIDTH = 400;
+	
+	//Set up the window
 	public void startWin(){
 		window.setSize(WIDTH, HEIGHT);
 		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -42,27 +47,58 @@ public class MainWin extends JFrame{
 		
 	}
 	
+	//Fill the content panels with content
 	public void mainScreenPanelMake(){
 		top.add(new JLabel("Polynomial:"));
 		top.add(new JTextField(17));
 		bottom.add(new JButton("Submit"));
 	}
+	public void startScreenPanelMake(){
+		JLabel polyC = new JLabel("Synthetic Division Calculator");
+//		polyC.setHorizontalAlignment(/2);
+		topIntro.add(polyC);
+		JButton start = new JButton("Start");
+		start.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+            	removeHomePanels();
+            	addMainPanels();
+            	fixPane();
+            }
+        });  
+		bodyIntro.add(start);
+		
+	}
+	
 	//Perform all the fixing actions 
 	public void fixPane(){
 		window.revalidate();
 		window.repaint();
 	}
+	//Adds the main panels
 	public void addMainPanels(){
 		window.add(top);
 		window.add(middle);
 		window.add(bottom);
 	}
+	//Remove the main panels
 	public void removeMainPanels(){
 		window.remove(top);
 		window.remove(middle);
 		window.remove(bottom);
 	}
 	
+	public void addHomePanels(){
+		window.add(topIntro);
+		window.add(bodyIntro);
+	}
+	
+	public void removeHomePanels(){
+		window.remove(topIntro);
+		window.remove(bodyIntro);
+	}
 	
 	
 	
